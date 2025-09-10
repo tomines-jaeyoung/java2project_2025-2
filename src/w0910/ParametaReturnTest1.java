22b5d200bcfa9326f180e123544f2617f5d0bd96
tomines-jaeyoung <tominesss12@gmail.com>
2025-09-10 09:26:46 +0900

    Reapply "리턴값과 매개변수 2개를 갖는 메소드 호출 연습"
    This reverts commit ed73877000fb7e2e6ec9c8e5a2086118213bba68.

---
+++ src/w0910/ParametaReturnTest1.java
@@ -0,0 +1,20 @@
+package w0910;
+
+import java.util.Scanner;
+
+public class ParametaReturnTest1 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        System.out.println("=== 2개의 정수의 합계를 구해드립니다. ===");
+        System.out.print("★ 정수1 입력 :");
+        int num1 = sc.nextInt();
+        System.out.print("★ 정수12 입력 :");
+        int num2 = sc.nextInt();
+        Calc1 calc1 = new Calc1();
+    int result = calc1.plus(num1, num2);
+        System.out.println("덧셈결과 =" + result);
+
+        sc.close();
+    }
+}
